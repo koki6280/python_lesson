c43032df7d44b7c02296ef62fd71b75c20a0c5be
林幸輝 <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-11-27 22:19:39 +0900

    dic

---
+++ dic_lesson.py
@@ -0,0 +1,20 @@
+x = {'a': 1}
+y = x
+y['a'] = 1000
+print(x)
+print(y)
+
+x = {'a': 1}
+y = x.copy()
+y['a'] = 1000
+print(x)
+print(y)
+
+print('##############')
+fruits = {
+    'apple': 100,
+    'banana': 200,
+    'orange': 300,
+}
+print(fruits['apple'])
+
