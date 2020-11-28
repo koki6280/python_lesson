ace7930b5ec21cae08ff8dbd3eb1b482b041f2a7
林幸輝 <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-11-29 00:09:56 +0900

    is

---
+++ code_lesson.py
@@ -0,0 +1,54 @@
+x = 10
+
+if x < 0:
+    print('negative')
+elif x == 0:
+    print('zero')
+else:
+    print('positive')
+
+a = 5
+b = 10
+
+if a > 0:
+    print('a is positive')
+    if b > 0:
+        print('b is positive')
+
+y = [1, 2, 3]
+x = 1
+
+if x in y:
+    print('in')
+
+if 100 not in y:
+    print('not in')
+
+# a = 1
+# b = 2
+
+# if not a == b:
+    # print('Not equal')
+
+# if a != b:
+    # print('Not equal')
+
+# is_ok = True
+is_ok = [1, 2, 3, 4]
+
+# False 0, 0.0, '', [], (), {}, set()
+if is_ok:
+    print('OK!')
+else:
+    print('No!')
+
+is_empty = None
+# print(help(is_empty))
+if is_empty is None:
+    print('None!!!')
+
+print(1 == True)
+print(True is True)
+
+
+
