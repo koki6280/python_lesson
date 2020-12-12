01a334a5d51e7a8ab0ab5cce159a727533dde6a7
林幸輝 <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-12-12 17:54:51 +0900

    propaty

--- object.py
+++ object.py
@@ -18,15 +18,39 @@ person.say_something()
 del person
 
 print('##########################')
+class Person(object):
+    def __init__(self, age=1):
+        self.age = age
+
 
 class Car(object):
+    def __init__(self, model=None):
+        self.model = model
     def run(self):
         print('run')
 
 class ToyotaCar(Car):
+    def run(self):
+        print('fast')
     pass
 
 class TeslaCar(Car):
+    def __init__(self,
+                 model='Model S',
+                 enable_auto_run=False):
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self, is_enable):
+        self._enable_auto_run = is_enable
+
+    def run(self):
+        print('super fast')
     def auto_run(self):
         print('auto run')
 
@@ -34,11 +58,16 @@ car = Car()
 car.run()
 
 print('######################')
-toyota_car = ToyotaCar()
+toyota_car = ToyotaCar('Lexus')
+print(toyota_car.model)
 toyota_car.run()
 print('######################')
 
-tesla_car = TeslaCar()
+tesla_car = TeslaCar('Model S')
+tesla_car.enable_auto_run = True
+print(tesla_car.enable_auto_run)
+
+print(tesla_car.model)
 tesla_car.run()
 tesla_car.auto_run()
 
